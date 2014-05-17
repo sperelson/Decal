c24d6d36ab1874ab49576c1cbb25efba355a4f66
Stephen Perelson <sperelson@gmail.com>
2014-05-17 23:31:44 +0200

    improve getComposer.sh with prompt to run

--- getComposer.sh
+++ getComposer.sh
@@ -3,3 +3,11 @@
 curl -sS https://getcomposer.org/installer | php
 # or 
 # php -r "readfile('https://getcomposer.org/installer');" | php
+
+echo "Run composer to install project dependencies?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) php composer.phar install; break;;
+        No ) exit;;
+    esac
+done
