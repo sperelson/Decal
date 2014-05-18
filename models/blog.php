b8f3f839e580d972c3b9a9c1caf4ba5d01c0a3cb
Stephen Perelson <sperelson@gmail.com>
2014-05-18 19:27:49 +0200

    more in the blog model

--- models/blog.php
+++ models/blog.php
@@ -1,5 +1,51 @@
 <?php
 
+include "../config/db.config";
+
 class Blog {
-	private $dbconn = null;
+	private static $dbconn = null;
+	
+	public function __construct() {
+	}
+
+	public static function getlist($page=1, $count=20, $published=1) {
+		$limit = $count;
+		$offset = (($page > 0 ? $page : 1) - 1) * $limit;
+
+		$con = self::dbCon();
+		$sqllistposts = <<<EOD
+select `id`, `title`, `created`, `published`
+from `posts`
+where `published` = ?
+limit ?, ?
+EOD;
+		$stmtblog = $con->prepare($sqllistposts);
+		$stmtblog->bind_param("iii", $published, $offset, $count);
+		$stmtblog->bind_result($id, $title, $created, $publishedflg);
+		$stmtblog->execute();
+		$posts[] = array();
+
+		while ($stmtblog->fetch()) {
+			$posts[] = array($id, $title, $created, $publishedflg);
+		}
+		$stmtblog->close();
+
+		return $posts;
+	}
+
+	/**
+	* dbCon
+	*
+	* Connects to the db
+	**/
+	private static function dbCon() {
+		if (!isset(self::$dbconn)) {
+			self::$dbconn = new mysqli(DB::server(), DB::user(), DB::pwd(), DB::dbname());
+		}
+		if (mysqli_connect_errno()) {
+			echo '';
+			die();
+		}		
+		return self::$dbconn;
+	}
 }
