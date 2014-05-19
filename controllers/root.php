5c171557fa4207173c503db02cfe634525ca4e53
Stephen Perelson <sperelson@gmail.com>
2014-05-19 02:52:23 +0200

    placeholder root view and test

--- controllers/root.php
+++ controllers/root.php
@@ -6,5 +6,14 @@ relurl: /
 ---
 
 $list = Blog::getlist(1, 20, 0);
-var_dump($list);
-echo "The first page people see - a list of blog posts";
+//var_dump($list);
+// echo "The first page people see - a list of blog posts";
+// create object
+
+$smarty = new Smarty;
+
+//$smarty->assign('name', 'george smith');
+//$smarty->assign('address', '45th & Harris');
+// display it
+{% assign partsofpath = page.url | remove_first: '/' | remove_first: '/' | split: '/' %}
+$smarty->display('{% for parts in partsofpath %}../{% endfor %}views/root.tpl');
