7d185ecc922b046484af43bb1be2eeabee2cd09f
Stephen Perelson <sperelson@gmail.com>
2014-05-19 02:51:41 +0200

    template users model

--- models/users.php
+++ models/users.php
@@ -4,4 +4,37 @@ require_once(__DIR__ . "/../config/db.config");
 
 class Users {
 	private $dbconn = null;
+
+	public static function getusers() {
+		$con = self::dbCon();
+		$sqlusers= "select `id`, `name`, `email` from `users`";
+
+		$stmt = $con->prepare($sqlusers);
+		$stmt->bind_result($id, $name, $email);
+		$stmt->execute();
+		$users = array();
+
+		while ($stmt->fetch()) {
+			$users = array($id, $name, $email);
+		}
+		$stmt->close();
+
+		return $users;
+	}
+
+	/**
+	* dbCon
+	*
+	* Connects to the db
+	**/
+	private static function dbCon() {
+		if (!isset(self::$dbconn)) {
+			self::$dbconn = new mysqli(DB::server(), DB::user(), DB::pwd(), DB::dbname());
+		}
+		if (mysqli_connect_errno()) {
+			echo '';
+			die();
+		}		
+		return self::$dbconn;
+	}
 }
