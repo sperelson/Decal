d0c8e65acc4d2e0d1b68a1167870f7e7ecc55c75
Stephen Perelson <sperelson@gmail.com>
2014-05-21 12:16:23 +0200

    minimum viable root controller

--- controllers/root.php
+++ controllers/root.php
@@ -5,15 +5,11 @@ permalink: /root/index.php
 relurl: /
 ---
 
-$list = Blog::getlist(1, 20, 0);
-//var_dump($list);
-// echo "The first page people see - a list of blog posts";
-// create object
+$posts = Blog::getlist(1, 20, 0);
 
 $smarty = new Smarty;
+$smarty->assign('posts', $posts);
 
-//$smarty->assign('name', 'george smith');
-//$smarty->assign('address', '45th & Harris');
 // display it
 {% assign partsofpath = page.url | remove_first: '/' | remove_first: '/' | split: '/' %}
 $smarty->display('{% for parts in partsofpath %}../{% endfor %}views/root.tpl');
